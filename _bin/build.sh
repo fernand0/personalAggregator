b985e8af0aa8931da8cd6fcfddcb1988dd058c4a
fernand0 <fernand0@elmundoesimperfecto.com>
2025-12-21 23:37:32 +0100

    Adding posts directory instead of posts

--- _bin/build.sh
+++ _bin/build.sh
@@ -75,7 +75,7 @@ export PATH="$HOME/gems/bin:$PATH"
 
 # bundle exec jekyll build >> $LOG_FILE 2>&1
 
-git add "$POSTS_DIR"/* >> $LOG_FILE 2>&1 # Use POSTS_DIR
+git add "$POSTS_DIR" >> $LOG_FILE 2>&1 # Use POSTS_DIR
 git commit -m "Publication: $(date +'%%Y-%%m-%%d %%H:%%M:%%S')" >> $LOG_FILE 2>&1
 git push >> $LOG_FILE 2>&1
 
